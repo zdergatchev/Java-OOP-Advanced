2808b5510c8a8cf397740542bc5bb2091baa8cb7
limerixtt123 <limerix@mail.bg>
2017-03-15 12:27:09 +0200

    Interfaces Lab

---
+++ 01InterfacesLab/SayHello/Bulgarian.java
@@ -0,0 +1,23 @@
+package SayHello;
+
+public class Bulgarian implements Person{
+    private String name;
+
+     public Bulgarian(String name){
+         this.setName(name);
+     }
+
+     private void setName(String name){
+         this.name = name;
+     }
+
+     @Override
+     public String getName(){
+         return null;
+     }
+
+     @Override
+    public String sayHello(){
+         return "Здравей";
+     }
+}
