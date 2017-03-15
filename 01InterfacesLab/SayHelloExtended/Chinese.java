23109f6539f7329088756755955c2ebdbef56ca8
limerixtt123 <limerix@mail.bg>
2017-03-15 12:28:10 +0200

    Interfaces Lab

---
+++ 01InterfacesLab/SayHelloExtended/Chinese.java
@@ -0,0 +1,12 @@
+package SayHelloExtended;
+
+public class Chinese extends BasePerson implements Person {
+    protected Chinese (String name){
+        super(name);
+    }
+
+    @Override
+    public String sayHello(){
+        return "Djydjybydjy";
+    }
+}
---
+++ 01InterfacesLab/SayHelloExtended/European.java
@@ -0,0 +1,12 @@
+package SayHelloExtended;
+
+public class European extends BasePerson implements Person {
+    protected European (String name){
+        super(name);
+    }
+
+    @Override
+    public String sayHello(){
+        return "Hello";
+    }
+}
