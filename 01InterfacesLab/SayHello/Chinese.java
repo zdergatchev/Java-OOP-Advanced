dbd5f9b723e0409d4c2038bf071ddb77cb74456b
limerixtt123 <limerix@mail.bg>
2017-03-15 12:27:20 +0200

    Interfaces Lab

---
+++ 01InterfacesLab/SayHello/Chinese.java
@@ -0,0 +1,23 @@
+package SayHello;
+
+public class Chinese implements Person{
+    private String name;
+
+    public Chinese(String name){
+        this.setName(name);
+    }
+
+    private void setName(String name){
+        this.name = name;
+    }
+
+    @Override
+    public String getName(){
+        return null;
+    }
+
+    @Override
+    public String sayHello(){
+        return "Djydjybydjy";
+    }
+}
